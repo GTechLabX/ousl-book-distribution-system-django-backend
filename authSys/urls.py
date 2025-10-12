734f75839596e60ea143a144475290bce00078bf
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2025-10-12 12:45:41 +0530

    created the urls , views and login and regisration views

---
+++ authSys/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'authSys'
+
+urlpatterns = [
+    path('', views.login, name="login"),
+    path('', views.registration, name="registration")
+]
