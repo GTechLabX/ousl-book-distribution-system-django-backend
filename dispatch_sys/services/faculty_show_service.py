7e70ea27eb1f2cadd6cbd6daeaf4399e0ebe0e9d
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2025-10-30 11:59:26 +0530

    impliment the get only 1 faculyt data

--- dispatch_sys/services/faculty_show_service.py
+++ dispatch_sys/services/faculty_show_service.py
@@ -1,3 +1,24 @@
+from dispatch_sys.models import Faculty
+from dispatch_sys.serializers.faculty_serializers import FacultySerializer
 
-def faculty_show_service(sender, data, callback, **kwargs):
-    pass
+
+def faculty_show_service(sender, callback, pk, **kwargs):
+    try:
+        faculty = Faculty.objects.get(pk=pk)
+        serializer = FacultySerializer(instance=faculty)
+        return callback({
+            "success": True,
+            "data": serializer.data
+        })
+
+    except Faculty.DoesNotExist:
+        return callback({
+            "success": False,
+            "errors": "Faculty does not exist"
+        })
+
+    except Exception as e:
+        return callback({
+            "success": False,
+            "errors": str(e)
+        })
